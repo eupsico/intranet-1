711a3f19517964dd36d63ca2f1833c3d1e9c7f89
marco-egito <eupsico@eupsico.org.br>
2025-09-09 17:32:27 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -138,7 +138,7 @@ document.addEventListener('DOMContentLoaded', function() {
             portal_voluntario: { 
                 titulo: 'Portal do Voluntário', 
                 descricao: 'Avisos, notícias e informações importantes para todos os voluntários.', 
-                url: '#announcements-section',
+                url: './pages/portal-voluntario.html',
                 roles: ['todos'],
                 icon: icons.intranet 
             },
