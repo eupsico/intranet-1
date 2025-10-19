3b2a82a723369d5cf0def1085e77ff805ab3d268
marco-egito <eupsico@eupsico.org.br>
2025-10-19 12:17:23 -0300

    Update modals.js

--- modulos/voluntario/js/meus-pacientes/modals.js
+++ modulos/voluntario/js/meus-pacientes/modals.js
@@ -108,7 +108,7 @@ function preencherFormularioMensagem(templateKey, templateTitle) {
     const formGroup = document.createElement("div");
     formGroup.className = "form-group";
     const label = document.createElement("label");
-    label.textContent = `Preencha o campo "${labelText}":`;
+    label.textContent = "Informe sua profissão:";
     label.htmlFor = `var-${nomeVariavel}`;
     const input = document.createElement("input");
 
