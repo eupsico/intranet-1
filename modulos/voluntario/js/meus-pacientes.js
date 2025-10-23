01a23ae67ba0ea9cde701afbb39c97299b6aae46
marco-egito <eupsico@eupsico.org.br>
2025-10-23 10:32:33 -0300

    novo

--- modulos/voluntario/js/meus-pacientes.js
+++ modulos/voluntario/js/meus-pacientes.js
@@ -1,18 +1,18 @@
 // Arquivo: /modulos/voluntario/js/meus-pacientes.js
-// Versão: 9.1 (Modularizado)
+// Versão: 9.2 (Modularizado - Refatoração Detalhe Paciente)
 
 import { initializeMeusPacientes } from "./meus-pacientes/data.js";
 
 export async function init(user, userData) {
-  // O container principal onde o acordeão será renderizado
-  const container = document.getElementById("pacientes-accordion-container");
+  // O container principal onde a lista de pacientes será renderizada
+  // O ID foi alterado de "pacientes-accordion-container" para "pacientes-list-container" no HTML.
+  const container = document.getElementById("pacientes-list-container");
   if (!container) {
     console.error(
-      "Container de pacientes não encontrado. (ID: pacientes-accordion-container)"
+      "Container de pacientes não encontrado. (ID: pacientes-list-container)"
     );
     return;
-  }
+  } // Chama a função inicializadora do módulo de dados, passando as dependências
 
-  // Chama a função inicializadora do módulo de dados, passando as dependências
   await initializeMeusPacientes(user, userData, container);
 }
