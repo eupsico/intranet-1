07a20de5e32a657e4ed943be3741b54266b2ae28
eupsico <eupsico@eupsico.org.br>
2025-12-02 17:23:46 -0300

    Update dashboard-reunioes.js

--- modulos/gestao/js/dashboard-reunioes.js
+++ modulos/gestao/js/dashboard-reunioes.js
@@ -510,9 +510,9 @@ function renderizarProximaReuniao(listaUnificada) {
           "pt-BR",
           { dateStyle: "short", timeStyle: "short" }
         )}</p>
-        <p class="mb-2 text-white"><span class="material-symbols-outlined" style="font-size:16px; color: white; vertical-align:middle">location_on ${
+        <p class="mb-2 text-white" style=color: white;><span class="material-symbols-outlined" style="font-size:16px; vertical-align:middle">location_on</span> ${
           proxima.local || "Online"
-        }</span></p>
+        }</p>
         <p class="fw-bold text-warning mb-0">${tempoTexto}</p>
     `;
 
